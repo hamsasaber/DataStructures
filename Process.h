04b7dc5806a0f0a74c3102b7430d0b0547003abb
salsabilmostafa <115428975+salsabilmostafa@users.noreply.github.com>
2023-04-15 02:27:45 -0700

    Add files via upload

---
+++ Process.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "NodeI_O.h"
+template <typename T>
+class Process
+{
+private:
+	int PID;
+	int AT;
+	int RT;
+	int CT;
+	int TT;
+	int TRT;
+	int WT;
+	bool NEW;
+	bool RDY;
+	bool RUN;
+	bool BLK;
+	bool TRM;
+	bool ORPH;
+	Node <T>* I_O[100];
+public:
+	Process(int id, int at, int rt, int ct, int tt);
+	int getPID();
+	int getAT();
+	int getRT();
+	int getCT();
+	int getTT();
+	int geti_o();
+	void seti_o(T i_o);
+	void setTRT();
+	int getTRT();
+	void setWT();
+	int getWT();
+	void setNEW(bool a);
+	bool getNEW();
+	void setRDY(bool b);
+	bool getRDY();
+	void setRUN(bool c);
+	bool getRUN();
+	void setBLK( bool d);
+	bool getBLK();
+	void setTRM(bool e);
+	bool getTRM();
+	void setORPH(bool f);
+	bool getORPH();
+};
+
