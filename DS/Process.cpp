76967e50a2cffcc5ca901dce988b70d43e27beeb
salsabilmostafa <115428975+salsabilmostafa@users.noreply.github.com>
2023-04-17 03:14:38 -0700

    Add files via upload

--- DS/Process.cpp
+++ DS/Process.cpp
@@ -1,1 +1,91 @@
 #include "Process.h"
+Process::Process(int id, int at, int rt, int ct, int tt)
+{
+	PID = id;
+	AT = at;
+	RT = rt;
+	CT = ct;
+	TT = tt;
+}
+int Process::getPID() { return PID; }
+int Process::getAT() { return AT; }
+int Process::getRT() { return RT; }
+int Process::getCT() { return CT; }
+int Process::getTT() { return TT; }
+void Process::seti_o(int i_o) { i_o = I_O; }
+int Process::geti_o() { return I_O; }
+
+void Process ::setTRT()
+{
+	TRT = TT - AT;
+}
+
+int Process::getTRT()
+{
+	return TRT;
+}
+
+
+void Process::setWT()
+{
+	WT = TRT - CT;
+}
+
+
+int Process::getWT()
+{
+	return WT;
+}
+
+void Process::setNEW(bool a)
+{
+	a = true;
+}
+
+
+bool Process::getNEW()
+{}
+
+void Process::setRDY(bool b)
+{
+	b = false;
+}
+
+
+bool Process::getRDY()
+{}
+
+
+void Process::setRUN(bool c)
+{
+	c = false;
+}
+
+bool Process::getRUN()
+{}
+
+
+void Process::setBLK(bool d)
+{
+	d = false;
+}
+
+bool Process::getBLK()
+{}
+
+void Process::setTRM(bool e)
+{
+	e = false;
+}
+
+
+bool Process::getTRM()
+{}
+
+void Process::setORPH(bool f)
+{
+	f = false;
+}
+
+bool Process::getORPH()
+{}
--- DS/Process.h
+++ DS/Process.h
@@ -1,9 +1,48 @@
 #pragma once
-template <typename T>
+#include "NodeI_O.h"
+#include "Queue.h"
+
 class Process
 {
+private:
+	int PID;
+	int AT;
+	int RT;
+	int CT;
+	int TT;
+	int TRT;
+	int WT;
+	bool NEW;
+	bool RDY;
+	bool RUN;
+	bool BLK;
+	bool TRM;
+	bool ORPH;
+	Queue <NodeI_O>I_O;
 public:
-	int getID();
-	T print();
+	Process(int id, int at, int rt, int ct, int tt);
+	int getPID();
+	int getAT();
+	int getRT();
+	int getCT();
+	int getTT();
+	int geti_o();
+	void seti_o(T i_o);
+	void setTRT();
+	int getTRT();
+	void setWT();
+	int getWT();
+	void setNEW(bool a);
+	bool getNEW();
+	void setRDY(bool b);
+	bool getRDY();
+	void setRUN(bool c);
+	bool getRUN();
+	void setBLK( bool d);
+	bool getBLK();
+	void setTRM(bool e);
+	bool getTRM();
+	void setORPH(bool f);
+	bool getORPH();
 };
 
